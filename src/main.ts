d0c396c86da39886961ad2e7c43ecb7524398865
solonskiy <solonskiyigor@gmail.com>
2024-06-28 13:51:12 +0300

    updated envs

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,19 @@
 import * as cookieParser from 'cookie-parser';
 
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('PORT');
 
   app.use(cookieParser());
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 
 bootstrap();
